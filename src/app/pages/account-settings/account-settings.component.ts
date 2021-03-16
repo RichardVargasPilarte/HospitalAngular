7af98031644228947d349a2ea4ab3b5df1a72c72
RichardVargasPilarte <ricardo.pilarte@gmail.com>
2021-03-16 16:23:30 -0600

    Fin Sección 7

---
+++ src/app/pages/account-settings/account-settings.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { SettingsService } from 'src/app/services/settings.service';
+
+@Component({
+  selector: 'app-account-settings',
+  templateUrl: './account-settings.component.html',
+  styleUrls: ['./account-settings.component.css']
+})
+export class AccountSettingsComponent implements OnInit {
+
+  constructor(private settingsServices: SettingsService) {
+  }
+
+  ngOnInit(): void {
+    this.settingsServices.checkCurrentTheme();
+  }
+
+  changeTheme( theme: string ): void {
+
+    this.settingsServices.changeTheme(theme);
+  }
+}
